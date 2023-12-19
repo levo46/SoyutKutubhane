7b266e9a56ebdad85727069717e389ec77c21289
levent581 <levent581@BPC03>
2023-12-19 11:35:53 +0300

    Proje dosyası ekle.

---
+++ SoyutKutubhane/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiraSizde104
+{
+    public abstract class Ortak
+    {
+        public uint SayfaSayisi { get; set; }
+        public string Adi { get; set; }
+        public ushort BasimYili { get; set; }
+        public abstract void Oku();
+        
+    }
+    public class Kitap : Ortak
+    {
+        public uint Turu { get; set; }
+        public override void Oku()
+        {
+            Console.WriteLine("Kitap okuma metodu gerçekleşti");
+        }
+    }
+    public class Dergi : Ortak
+    {
+        public uint SayiNo { get; set; }
+        public override void Oku()
+        {
+            Console.WriteLine("Dergi okuma işlemi gerçekleşti");
+        }
+
+    }
+    public class Ansiklopedi : Ortak
+    {
+        public string CiltNo { get; set; }
+        public override void Oku()
+        {
+            Console.WriteLine("Ansiklopedi okuma işlemi gerçekleşti");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ansiklopedi ansiklopedi = new Ansiklopedi();
+            ansiklopedi.Oku();
+            Console.WriteLine("=========================");
+            Kitap kitap = new Kitap();
+            kitap.Oku();
+            Console.WriteLine("=========================");
+            Dergi dergi = new Dergi();
+            dergi.Oku();
+            Console.WriteLine("=========================");
+        }
+    }
+}
---
+++ SoyutKutubhane/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("SoyutKutubhane")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SoyutKutubhane")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("fff8a3d8-e981-4379-ab3b-7e37a9e8974e")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
